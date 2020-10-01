2d95762ad83d2ba00baa6f28d4c6c50e53b4d945
Tore Berven <tore.berven@student.uib.no>
2020-10-02 01:14:11 +0200

    Added Classes for handling a duration of time
    ITimeDuration.java
    * Interface for TimeDuration.java
    * Provides static functions for converting a number of
      seconds, or a string of the format "Days:x,Hours:y,Minutes:z,Seconds:a",
      to the time units: days, hours, minutes and seconds.
    
    TimeDuration.java
    * Implements the ITimeDuration interface.
    * Holds an ArrayList<Integer> of the aforementioned time units.
    * Provides functionality for adding or subtracting
      time units to/from a TimeDuration;
    * Method for counting down one second at a time.
    * Method for creating a ZonedDateTime based on the current system time,
      with the added time duration of the TimeDuration object.
    * It is also possible to turn a TimeDuration into a string and back again!
      (toString() has valid format for the static interface method)

---
+++ src/main/java/Projects/ProjectB/time/ITimeDuration.java
@@ -0,0 +1,201 @@
+package Projects.ProjectB.time;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+public interface ITimeDuration {
+
+    int HOURS_IN_A_FRESH_DAY = 23;
+    int MINUTES_IN_A_FRESH_HOUR = 59;
+    int SECONDS_IN_A_FRESH_MINUTE = 59;
+
+    int NUMBER_OF_SECONDS_IN_A_MINUTE = 60;
+    int NUMBER_OF_MINUTES_IN_AN_HOUR = 60;
+    int NUMBER_OF_HOURS_IN_A_DAY = 24;
+
+    /**
+     * Retrieves the number of days from the TimeDuration
+     *
+     * @return The number of days
+     */
+    Integer getDays();
+
+    /**
+     * Retrieves the number of hours from the TimeDuration
+     *
+     * @return The number of hours
+     */
+    Integer getHours();
+
+    /**
+     * Retrieves the number of minutes from the TimeDuration
+     *
+     * @return The number of minutes
+     */
+    Integer getMinutes();
+
+    /**
+     * Retrieves the number of seconds from the TimeDuration
+     *
+     * @return The number of seconds
+     */
+    Integer getSeconds();
+
+    /**
+     * Update the value of the time unit days, of the TimeDuration.
+     *
+     * @param newValue The new value for the time unit days.
+     */
+    void setDays(Integer newValue);
+
+    /**
+     * Update the value of the time unit hours, of the TimeDuration.
+     *
+     * @param newValue The new value for the time unit hours.
+     */
+    void setHours(Integer newValue);
+
+    /**
+     * Update the value of the time unit minutes, of the TimeDuration.
+     *
+     * @param newValue The new value for the time unit minutes.
+     */
+    void setMinutes(Integer newValue);
+
+    /**
+     * Update the value of the time unit seconds, of the TimeDuration.
+     *
+     * @param newValue The new value for the time unit seconds.
+     */
+    void setSeconds(Integer newValue);
+
+    /**
+     * Subtract the given number of days from the TimeDuration.
+     *
+     * @param daysToSubtract The number of days to subtract
+     */
+    void reduceDaysBy(Integer daysToSubtract);
+
+    /**
+     * Subtract the given number of hours from the TimeDuration.
+     *
+     * @param hoursToSubtract The number of hours to subtract
+     */
+    void reduceHoursBy(Integer hoursToSubtract);
+
+    /**
+     * Subtract the given number of minutes from the TimeDuration.
+     *
+     * @param minutesToSubtract The number of minutes to subtract
+     */
+    void reduceMinutesBy(Integer minutesToSubtract);
+
+    /**
+     * Subtract the given number of seconds from the TimeDuration.
+     *
+     * @param secondsToSubtract The number of seconds to subtract
+     */
+    void reduceSecondsBy(Integer secondsToSubtract);
+
+    /**
+     * Add the given number of days to the TimeDuration.
+     *
+     * @param daysToAdd The number of days to add
+     */
+    void increaseDaysBy(Integer daysToAdd);
+
+    /**
+     * Add the given number of hours to the TimeDuration.
+     *
+     * @param hoursToAdd The number of hours to add
+     */
+    void increaseHoursBy(Integer hoursToAdd);
+
+    /**
+     * Add the given number of minutes to the TimeDuration.
+     *
+     * @param minutesToAdd The number of minutes to add
+     */
+    void increaseMinutesBy(Integer minutesToAdd);
+
+    /**
+     * Add the given number of seconds to the TimeDuration.
+     *
+     * @param secondsToAdd The number of seconds to add
+     */
+    void increaseSecondsBy(Integer secondsToAdd);
+
+    /**
+     * Prints the time units of the TimeDuration.
+     */
+    void printTimeDuration();
+
+    /**
+     * Updates the TimeDuration to be one second less than it was.
+     */
+    void countDownOneSecond();
+
+    /**
+     * Get the ZonedDateTime for today, plus the added TimeDuration.
+     *
+     * @return A ZonedDateTime with the day, hour, minute and seconds of
+     * the TimeDuration added to the current system time.
+     */
+    ZonedDateTime futureZonedDateTimeFromTimeDuration();
+
+    /**
+     * Takes a number of seconds, and converts it to a
+     * list of time units that comprises the TimeDuration
+     * (days, hours, minutes and seconds).
+     *
+     * @param timeInSeconds The time in seconds to be converted to time units.
+     * @return A List of time units, that together comprises the TimeDuration:<br>
+     * First element is the number of days.<br>
+     * Second element is the number of hours.<br>
+     * Third element is the number of minutes.<br>
+     * Fourth element is the number of seconds.
+     * @throws ArithmeticException if the provided number of seconds  is too large<br>
+     *                             (timeInSeconds > 185.542.587.187.199)
+     */
+    static ArrayList<Integer> secondsToTimeUnits(long timeInSeconds) {
+        long days, hours, minutes, seconds;
+        minutes = timeInSeconds / NUMBER_OF_SECONDS_IN_A_MINUTE;
+        hours = minutes / NUMBER_OF_MINUTES_IN_AN_HOUR;
+        days = hours / NUMBER_OF_HOURS_IN_A_DAY;
+        seconds = timeInSeconds - (minutes * NUMBER_OF_SECONDS_IN_A_MINUTE);
+        minutes = minutes % NUMBER_OF_MINUTES_IN_AN_HOUR;
+        hours = hours % NUMBER_OF_HOURS_IN_A_DAY;
+        ArrayList<Integer> timeUnits = new ArrayList<>();
+        timeUnits.add(Math.toIntExact(days)); // Fails if number of days exceed size of Integer
+        timeUnits.add(Math.toIntExact(hours));
+        timeUnits.add(Math.toIntExact(minutes));
+        timeUnits.add(Math.toIntExact(seconds));
+        return timeUnits;
+    }
+
+    /**
+     * Takes a String of time units, and turns it into a TimeDuration.
+     *
+     * @param timeInTimeUnits A String of time units of the format<br>
+     *                        "Days:x,Hours:y,Minutes:z,Seconds:a"
+     * @return The TimeDuration representation of the given time units.
+     */
+    static ITimeDuration timeDurationFromStringOfTimeUnits(String timeInTimeUnits) {
+        String[] timeUnits = timeInTimeUnits.split(",");
+        for (int i = 0; i < timeUnits.length; i++) {
+            timeUnits[i] = timeUnits[i].replaceAll("[^\\d]", ""); // Remove all characters not digits
+        }
+        int days, hours, minutes, seconds;
+        days = Integer.parseInt(timeUnits[0]);
+        hours = Integer.parseInt(timeUnits[1]);
+        minutes = Integer.parseInt(timeUnits[2]);
+        seconds = Integer.parseInt(timeUnits[3]);
+        ITimeDuration timeFromString = new TimeDuration(0L);
+        timeFromString.setDays(days);
+        timeFromString.setHours(hours);
+        timeFromString.setMinutes(minutes);
+        timeFromString.setSeconds(seconds);
+        return timeFromString;
+    }
+
+}
---
+++ src/main/java/Projects/ProjectB/time/TimeDuration.java
@@ -0,0 +1,250 @@
+package Projects.ProjectB.time;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+public class TimeDuration implements ITimeDuration {
+
+    /**
+     * A List of time units, that together comprises the TimeDuration:<br>
+     * First element is the number of days.<br>
+     * Second element is the number of hours.<br>
+     * Third element is the number of minutes.<br>
+     * Fourth element is the number of seconds.
+     */
+    private final ArrayList<Integer> timeDuration;
+
+    public TimeDuration(Long timeInSeconds) {
+        this.timeDuration = ITimeDuration.secondsToTimeUnits(timeInSeconds);
+    }
+
+    @Override
+    public Integer getDays() {
+        return this.timeDuration.get(0);
+    }
+
+    @Override
+    public Integer getHours() {
+        return this.timeDuration.get(1);
+    }
+
+    @Override
+    public Integer getMinutes() {
+        return this.timeDuration.get(2);
+    }
+
+    @Override
+    public Integer getSeconds() {
+        return this.timeDuration.get(3);
+    }
+
+    @Override
+    public void setDays(Integer newValue) {
+            this.timeDuration.set(0, newValue);
+    }
+
+    @Override
+    public void setHours(Integer newValue) {
+        if (newValue < 0) {
+            reduceHoursBy(newValue * (-1));
+        } else if (newValue >= NUMBER_OF_HOURS_IN_A_DAY) {
+            increaseHoursBy(newValue);
+        } else {
+            this.timeDuration.set(1, newValue);
+        }
+    }
+
+    @Override
+    public void setMinutes(Integer newValue) {
+        if (newValue < 0) {
+            reduceMinutesBy(newValue * (-1));
+        } else if (newValue >= NUMBER_OF_MINUTES_IN_AN_HOUR) {
+            increaseMinutesBy(newValue);
+        } else {
+            this.timeDuration.set(2, newValue);
+        }
+    }
+
+    @Override
+    public void setSeconds(Integer newValue) {
+        if (newValue < 0) {
+            reduceSecondsBy(newValue * (-1));
+        } else if (newValue >= NUMBER_OF_SECONDS_IN_A_MINUTE) {
+            increaseSecondsBy(newValue);
+        } else {
+            this.timeDuration.set(3, newValue);
+        }
+    }
+
+    @Override
+    public void reduceDaysBy(Integer daysToSubtract) {
+        int days = this.getDays();
+        int newValueOfDays = days - daysToSubtract;
+        if (daysToSubtract < 0) {
+            increaseDaysBy(daysToSubtract * (-1)); // Someone called the wrong function...
+        } else {
+            setDays(newValueOfDays);
+        }
+    }
+
+    @Override
+    public void reduceHoursBy(Integer hoursToSubtract) {
+        int hours = this.getHours();
+        int newValueOfHours = hours - hoursToSubtract;
+        if (hoursToSubtract < 0) {
+            increaseHoursBy(hoursToSubtract * (-1)); // Someone called the wrong function...
+        } else if (newValueOfHours < 0) {
+            int daysToSubtract = 1 + (Math.abs(newValueOfHours) / NUMBER_OF_HOURS_IN_A_DAY);
+            reduceDaysBy(daysToSubtract);
+            int remainingHours = newValueOfHours + (daysToSubtract * NUMBER_OF_HOURS_IN_A_DAY);
+            setHours(remainingHours);
+        } else {
+            setHours(newValueOfHours);
+        }
+    }
+
+    @Override
+    public void reduceMinutesBy(Integer minutesToSubtract) {
+        int minutes = this.getMinutes();
+        int newValueOfMinutes = minutes - minutesToSubtract;
+        if (minutesToSubtract < 0) {
+            increaseMinutesBy(minutesToSubtract * (-1)); // Someone called the wrong function...
+        } else if (newValueOfMinutes < 0) {
+            int hoursToSubtract = 1 + (Math.abs(newValueOfMinutes) / NUMBER_OF_MINUTES_IN_AN_HOUR);
+            reduceHoursBy(hoursToSubtract);
+            int remainingMinutes = newValueOfMinutes + (hoursToSubtract * NUMBER_OF_MINUTES_IN_AN_HOUR);
+            setMinutes(remainingMinutes);
+        } else {
+            setMinutes(newValueOfMinutes);
+        }
+    }
+
+    @Override
+    public void reduceSecondsBy(Integer secondsToSubtract) {
+        int seconds = this.getSeconds();
+        int newValueOfSeconds = seconds - secondsToSubtract;
+        if (secondsToSubtract < 0) {
+            increaseSecondsBy(secondsToSubtract * (-1)); // Someone called the wrong function...
+        } else if (newValueOfSeconds < 0) {
+            int minutesToSubtract = 1 + (Math.abs(newValueOfSeconds) / NUMBER_OF_SECONDS_IN_A_MINUTE);
+            reduceMinutesBy(minutesToSubtract);
+            int remainingSeconds = newValueOfSeconds + (minutesToSubtract * NUMBER_OF_SECONDS_IN_A_MINUTE);
+            setSeconds(remainingSeconds);
+        } else {
+            setSeconds(newValueOfSeconds);
+        }
+    }
+
+    @Override
+    public void increaseDaysBy(Integer daysToAdd) {
+        int days = this.getDays();
+        int newValueOfDays = days + daysToAdd;
+        if (daysToAdd < 0) {
+            reduceDaysBy(daysToAdd * (-1)); // Someone called the wrong function...
+        } else {
+            setDays(newValueOfDays);
+        }
+    }
+
+    @Override
+    public void increaseHoursBy(Integer hoursToAdd) {
+        int hours = this.getHours();
+        int newValueOfHours = hours + hoursToAdd;
+        if (hoursToAdd < 0) {
+            reduceHoursBy(hoursToAdd * (-1)); // Someone called the wrong function...
+        } else if (newValueOfHours >= NUMBER_OF_HOURS_IN_A_DAY) {
+            int daysToAdd = newValueOfHours / NUMBER_OF_HOURS_IN_A_DAY;
+            increaseDaysBy(daysToAdd);
+            int remainingHours = newValueOfHours - (daysToAdd * NUMBER_OF_HOURS_IN_A_DAY);
+            setHours(remainingHours);
+        } else {
+            setHours(newValueOfHours);
+        }
+    }
+
+    @Override
+    public void increaseMinutesBy(Integer minutesToAdd) {
+        int minutes = this.getMinutes();
+        int newValueOfMinutes = minutes + minutesToAdd;
+        if (minutesToAdd < 0) {
+            reduceMinutesBy(minutesToAdd * (-1)); // Someone called the wrong function...
+        } else if (newValueOfMinutes >= NUMBER_OF_MINUTES_IN_AN_HOUR) {
+            int hoursToAdd = newValueOfMinutes / NUMBER_OF_MINUTES_IN_AN_HOUR;
+            increaseHoursBy(hoursToAdd);
+            int remainingMinutes = newValueOfMinutes - (hoursToAdd * NUMBER_OF_MINUTES_IN_AN_HOUR);
+            setMinutes(remainingMinutes);
+        } else {
+            setMinutes(newValueOfMinutes);
+        }
+    }
+
+    @Override
+    public void increaseSecondsBy(Integer secondsToAdd) {
+        int seconds = this.getSeconds();
+        int newValueOfSeconds = seconds + secondsToAdd;
+        if (secondsToAdd < 0) {
+            reduceSecondsBy(secondsToAdd * (-1)); // Someone called the wrong function...
+        } else if (newValueOfSeconds >= NUMBER_OF_SECONDS_IN_A_MINUTE) {
+            int minutesToAdd = newValueOfSeconds / NUMBER_OF_SECONDS_IN_A_MINUTE;
+            increaseMinutesBy(minutesToAdd);
+            int remainingSeconds = newValueOfSeconds - (minutesToAdd * NUMBER_OF_SECONDS_IN_A_MINUTE);
+            setSeconds(remainingSeconds);
+        } else {
+            setSeconds(newValueOfSeconds);
+        }
+    }
+
+    @Override
+    public void printTimeDuration() {
+        System.out.println(this.toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Days: " + this.getDays()
+                + ", hours: " + this.getHours()
+                + ", minutes: " + this.getMinutes()
+                + ", seconds: " + this.getSeconds();
+    }
+
+    @Override
+    public void countDownOneSecond() {
+        Integer seconds = this.getSeconds();
+        if (seconds - 1 < 0) {
+            Integer minutes = getMinutes();
+            if (minutes - 1 < 0) {
+                Integer hours = getHours();
+                if (hours - 1 < 0) {
+                    Integer days = getDays();
+                    setDays(days - 1);
+                    setHours(HOURS_IN_A_FRESH_DAY);
+                } else {
+                    setHours(hours - 1);
+                }
+                setMinutes(MINUTES_IN_A_FRESH_HOUR);
+            } else {
+                setMinutes(minutes - 1);
+            }
+            setSeconds(SECONDS_IN_A_FRESH_MINUTE);
+        } else {
+            setSeconds(seconds - 1);
+        }
+    }
+
+    @Override
+    public ZonedDateTime futureZonedDateTimeFromTimeDuration() {
+        long days, hours, minutes, seconds;
+        days = this.getDays().longValue();
+        hours = this.getHours().longValue();
+        minutes = this.getMinutes().longValue();
+        seconds = this.getSeconds().longValue();
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();
+        zonedDateTime = zonedDateTime.plusDays(days);
+        zonedDateTime = zonedDateTime.plusHours(hours);
+        zonedDateTime = zonedDateTime.plusMinutes(minutes);
+        zonedDateTime = zonedDateTime.plusSeconds(seconds);
+        return zonedDateTime;
+    }
+
+
+}
