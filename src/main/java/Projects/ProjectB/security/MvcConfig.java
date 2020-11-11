36974ef2761e79ec6f2a09db196a57efe24b0dc8
Tore Berven <tore.berven@student.uib.no>
2020-11-11 12:45:38 +0100

    nothing special

--- src/main/java/Projects/ProjectB/security/MvcConfig.java
+++ src/main/java/Projects/ProjectB/security/MvcConfig.java
@@ -1,8 +1,8 @@
 package Projects.ProjectB.security;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -10,7 +10,7 @@ public class MvcConfig implements WebMvcConfigurer {
 
     public void addViewControllers(ViewControllerRegistry registry) {
         // TODO: Should be updated to include actual webpages before production.
-        //registry.addViewController("/").setViewName("home");
+      //  registry.addViewController("/index").setViewName("index");
         registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
        // registry.addViewController("/").setViewName("index");
